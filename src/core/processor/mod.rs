e7ba18433b986a090435bbd2e0569b2d0881df07
Gregory Tanaka <gregoryftanaka@gmail.com>
2020-07-28 22:45:48 -0600

    Define and implement more opcodes and operations

--- src/core/processor/mod.rs
+++ src/core/processor/mod.rs
@@ -19,6 +19,7 @@ impl Default for Processor {
 }
 
 impl Processor {
+    const F: u8 = 0xF;
     // run a single CPU cycle
     pub fn step(&self) {
         // Fetch Opcode
@@ -47,19 +48,22 @@ impl Processor {
                 }
             }
             0x1 =>  { //  1NNN - goto NNN;
+                self::goto(nnn);
             }
             0x2 =>  { //  2NNN - *(0xNNN)()
 
             }
             0x3 => { // 3XNN - if(Vx==NN)
-                self::skip_on_equal(x, nn);
+                let vx = self::V[x];
+                self::skip_on_equal(vx, nn);
             }
             0x4 => { // 4XNN - if(Vx!=NN)
-                self::skip_on_unequal(x, nn);
+                let vx = self::V[x];
+                self::skip_on_unequal(vx, nn);
             }
             0x5 => {
                 if n == 0x0  {// 5XY0 - if(Vx==Vy)
-                    self::skip_on_equal_vars(x, y);
+                    self::skip_on_equal(x, y);
                 } else {
                     println!("invalid opcode")
                 }
@@ -68,47 +72,73 @@ impl Processor {
                 self::set_register(x, nn);
             }
             0x7 => { // 7XNN - Vx += NN
-
+                self::add_to_register(x, nn)
             }
             0x8 => { // 8XY... bit ops and math
                 match n {
                     0x0 => { // 8XY0 - Vx=Vy
                         let vy = self::V[y];
                         self::set_register(x, vy);
-                }
+                    }
                     0x1 => { // 8XY1 - Vx=Vx|Vy
-
-                }
+                        let z = self::V[x] | self::V[y];
+                        self::set_register(x, z);
+                    }
                     0x2 => { // 8XY2 - Vx=Vx&Vy
-
-                }
+                        let z = self::V[x] & self::V[y];
+                        self::set_register(x, z);
+                    }
                     0x3 => { // 8XY3 - Vx=Vx^Vy
-
-                }
+                        let z = self::V[x] ^ self::V[y];
+                        self::set_register(x, z);
+                    }
                     0x4 => { // 8XY4 - Vx += Vy
-
-                }
+                        self::add_to_register(x, self::V[y]);
+                        //TODO figure out carry
+                        let carry = ????;
+                        if carry {
+                            self::set_register(self::F, 1);
+                        } else {
+                            self::set_register(self::F, 0);
+                        }
+                    }
                     0x5 => { // 8XY5 - Vx -= Vy
-
-                }
+                        self::add_to_register(x, self::V[y]);
+                        //TODO figure out borrow
+                        let borrow = ????;
+                        if borrow {
+                            self::set_register(self::F, 0);
+                        } else {
+                            self::set_register(self::F, 1);
+                        }
+                    }
                     0x6 => { // 8XY6 - Vx>>=1
-
-                }
+                        //get least significant bit
+                        let lsb = (self::V[x] & 0x1);
+                        self::set_register(self::F, lsb);
+                        self::shift_right(x);
+                    }
                     0x7 => { // 8XY7 - Vx=Vy-Vx
-
-                }
+                        let z = self::V[y] - self::V[x];
+                        self::set_register(x, z);
+                    }
                     0xE => { // 8XYE - Vx<<=1
-                }
+                        // get most significant bit, 0x80 == 1000 0000
+                        let msb = (self::V[x] & 0x80);
+                        self::set_register(self::F, msb);
+                        self::shift_left(x);
+                    }
                     _ => {
                         println!("invalid opcode")
-                }
+                    }
                 }
             }
             0xA =>  { // ANNN - I = NNN
-
+                self::set_i(nnn);
             }
             0xB =>  { // BNNN - PC=V0+NNN
-
+                let address = self::V[0] + nnn;
+                self::goto(address);
             }
             0xC => { // CXNN - Vx=rand()&NN
 
@@ -153,7 +183,13 @@ impl Processor {
                     }
                     0x65 => { // FX65 - reg_load(Vx,&I)
                     }
+                    _ => {
+                        println!("invalid opcode");
+                    }
                 }
+            }
+            _ => {
+                println!("invalid opcode");
             }
         }
     }
@@ -167,30 +203,39 @@ impl Processor {
         self.PC = address;
     }
 
-    // 3XNN
-    fn skip_on_equal(&mut self, register: u8, value: u8) {
-        if self.V[register] == value {
+    // 3XNN, 5XY0
+    fn skip_on_equal(&mut self, i: u8, j: u8) {
+        if i == j {
             self.PC += 2;
         }
     }
 
     // 4XNN
-    fn skip_on_unequal(&mut self, register: u8, value: u8) {
-        if self.V[register] != value {
+    fn skip_on_unequal(&mut self, i: u8, j: u8) {
+        if i != j {
             self.PC += 2;
         }
     }
 
-    // 5XY0
-    fn skip_on_equal_vars(&mut self, x: u8, y: u8) {
-        if x == y {
-            self.PC += 2;
-        }
-    }
-
-    // 6XNN, 8XY0(???)
+    // 6XNN, 8XY0
     fn set_register(&mut self, register: u8, value: u8) {
         //TODO does this need any validation?
         self.V[register] = value;
+    }
+
+    fn add_to_register(&mut self, register: u8, value: u8) {
+        self.V[register] += value;
+    }
+
+    fn shift_right(&mut self, register: u8) {
+        self.V[register] >> 1;
+    }
+
+    fn shift_left(&mut self, register: u8) {
+        self.V[register] << 1;
+    }
+
+    fn set_i(&mut self, value: u16) {
+        self::I = value;
     }
 }
