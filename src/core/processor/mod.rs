405d23ed09bc213a0efad5afc92bfbbb923dae08
Gregory Tanaka <gregoryftanaka@gmail.com>
2020-11-11 16:48:15 -0700

    DYXN opcode working now

--- src/core/processor/mod.rs
+++ src/core/processor/mod.rs
@@ -42,6 +42,7 @@ impl Processor {
         self.decode_exec(op);
         // Decrement timers
         self.timers();
+        self.PC += 2;
     }
 
     pub fn timers(&mut self) {
@@ -57,15 +58,11 @@ impl Processor {
     // https://tobiasvl.github.io/blog/write-a-chip-8-emulator/#decode
     fn decode_exec(&mut self, op: u16) {
         let t = (op & 0xF000) >> 12; // first nibble
-        let x = (op & 0xF00) >> 8; // second nibble - used to look up one of the 16 registers
-        let y = (op & 0xF0) >> 4; // third nibble - also used to look up one of the 16 registers
+        let x = ((op & 0xF00) >> 8) as usize; // second nibble - used to look up one of the 16 registers
+        let y = ((op & 0xF0) >> 4) as usize; // third nibble - also used to look up one of the 16 registers
         let n = op & 0xF; // fourth nibble
         let nn = (op & 0xFF) as u8; // second byte - 8-bit immediate number
         let nnn = op & 0xFFF; // second, third and fourth nibbles - 12-bit immediate memory address.
-
-        //convinient for accessing V register
-        let ix = x as usize;
-        let iy = y as usize;
 
         match t {
             0x0 => {
@@ -93,13 +90,13 @@ impl Processor {
             }
             0x3 => { // 3XNN - if(Vx==NN)
                 //self.skip_on_equal(x, nn);
-                if self.V[ix] == nn {
+                if self.V[x] == nn {
                     self.PC += 2;
                 }
             }
             0x4 => { // 4XNN - if(Vx!=NN)
                 //self.skip_on_unequal(x, nn);
-                if self.V[ix] != nn {
+                if self.V[x] != nn {
                     self.PC += 2;
                 }
             }
@@ -108,99 +105,105 @@ impl Processor {
                     0x0 => {// 5XY0 - if(Vx==Vy)
                         //let vy = self.V.read_u16(y);
                         //self.skip_on_equal(x, vy);
-                        if self.V[ix] == self.V[iy] {
+                        if self.V[x] == self.V[y] {
                             self.PC += 2;
                         }
                     }
                     _ =>{
-                        println!("invalid opcode")
+                        println!("invalid opcode {}", op)
                     }
                 }
             }
             0x6 => { // 6XNN - Vx = NN
-                self.V[ix] = nn;
+                self.V[x] = nn;
             }
             0x7 => { // 7XNN - Vx += NN
-                self.V[ix] = self.V[ix] + nn;
+                self.V[x] = self.V[x] + nn;
             }
             0x8 => {
                 // 8XY... bit ops and math
                 match n {
                     0x0 => { // 8XY0 - Vx=Vy
-                        self.V[ix] = self.V[iy];
+                        self.V[x] = self.V[y];
                     }
                     0x1 => { // 8XY1 - Vx=Vx|Vy
-                        let z = self.V[ix] | self.V[iy];
+                        let z = self.V[x] | self.V[y];
 
-                        self.V[ix] = z;
+                        self.V[x] = z;
                     }
                     0x2 => { // 8XY2 - Vx=Vx&Vy
-                        let z = self.V[ix] & self.V[iy];
+                        let z = self.V[x] & self.V[y];
 
-                        self.V[ix] = z;
+                        self.V[x] = z;
                     }
                     0x3 => { // 8XY3 - Vx=Vx^Vy
-                        let z = self.V[ix] ^ self.V[iy];
+                        let z = self.V[x] ^ self.V[y];
 
-                        self.V[ix] = z;
+                        self.V[x] = z;
                     }
                     0x4 => { // 8XY4 - Vx += Vy
-                        let z = self.V[ix] + self.V[iy];
+                        //TODO idk if this is the right way to handle overflow
+                        let z = (self.V[x] + self.V[y]) as u16;
                         if (z) > 255 {
                             self.V[0xF] = 1;
                         } else {
                             self.V[0xF] = 0;
                         }
-                        self.V[ix] = z & 0xFF;
+                        self.V[x] = z as u8 & 0xFF;
                     }
                     0x5 => { // 8XY5 - Vx -= Vy
-                        let z = self.V[ix] - self.V[iy];
+                        //TODO idk if this is the right way to handle uderflow either
+                        let vx = self.V[x] as i16;
+                        let vy = self.V[y] as i16;
+                        let z = (vx - vy) as i16;
                         if z >= 0 {
                             self.V[0xF] = 1;
                         } else {
                             self.V[0xF] = 0;
                         }
-                        self.V[ix] = z & 0xFF;
+                        self.V[x] = z as u8 & 0xFF;
                     }
                     0x6 => { // 8XY6 - Vx>>=1
                         //get least significant bit
-                        let lsb = self.V[ix] & 0x1;
+                        let lsb = self.V[x] & 0x1;
                         //f is our Special register
                         self.V[0xF] = lsb;
 
                         //self.V.shift_right(x);
-                        self.V[ix] = self.V[ix] >> 1;
+                        self.V[x] = self.V[x] >> 1;
                     }
                     0x7 => { // 8XY7 - Vx=Vy-Vx
-                        let z = self.V[iy] - self.V[ix];
+                        let vx = self.V[x] as i16;
+                        let vy = self.V[y] as i16;
+                        let z = (vy - vx) as i16;
                         if z >= 0 {
                             self.V[0xF] = 1;
                         } else {
                             self.V[0xF] = 0;
                         }
-                        self.V[ix] = z & 0xFF;
+                        self.V[x] = z as u8 & 0xFF;
                     }
                     0xE => { // 8XYE - Vx<<=1
                         //get most significant bit, 0x80 == 1000 0000
-                        let msb = self.V[ix] & 0x80;
+                        let msb = self.V[x] & 0x80;
                         //f is our Special register
                         self.V[0xF] = msb;
 
                         //self.V.shift_left(x);
-                        self.V[ix] = self.V[ix] << 1;
+                        self.V[x] = self.V[x] << 1;
                     }
-                    _ => println!("invalid opcode"),
+                    _ => println!("invalid opcode {}", op),
                 }
             }
             0x9 => {
                 match n {
                     0x0 => { // 9XY0 - if(Vx!=Vy)
                         //skip_on_unequal
-                        if self.V[ix] != self.V[iy] {
+                        if self.V[x] != self.V[y] {
                             self.PC += 2;
                         }
                     }
-                    _ => println!("invalid opcode"),
+                    _ => println!("invalid opcode {}", op),
                 }
             }
             0xA => { // ANNN - I = NNN
@@ -213,28 +216,33 @@ impl Processor {
             0xC => { // CXNN - Vx=rand()&NN
                 let mut rng = thread_rng();
                 let z: u8 = rng.gen::<u8>() & nn;
-                self.V[ix] = z;
+                self.V[x] = z;
             }
             0xD => { // DXYN - draw(Vx,Vy,N)
                 // set draw flag to refresh screen and reset F register
-                println!("DXYN");
+                //println!("DXYN");
                 self.V[0xF] = 0;
                 self.draw_flag = true;
                 // https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
                 // for n rows of sprite data
                 for yline in 0..n {
-                    //get sprite data from memory
-                    let row = self.Memory.read_u8(self.I + yline);
-                    let yc = (self.V[iy] + yline as u8) % 32;//default height
+                    //get sprite data from memory -- 8*y because 8 is width of 
+                    //sprite and we want to skip bits we've already checked
+                    let row_byte = self.Memory.read_u8(self.I + (8*yline));
+                    // set y coordinate
+                    let yc = (self.V[y] + yline as u8) % 32;//default height
+                    //All sprites are 8 wide
                     for xline in 0..8 {
-                        let xc = (self.V[ix] + xline) % 64;//default width
-                        let fb = ((yc * 32) + xc) as usize;
-                        let pixel = self.Display.frame_buffer[fb];
-                        //flip pixel
-                        if pixel == 1 {
+                        // set x coordinate
+                        let xc = (self.V[x] + xline) % 64;//default width
+                        let fb = (yc as usize * 32) + xc as usize;
+                        let pixel = row_byte & (0x80 >> xline);
+                        // if pixel is set, flip it
+                        // register colision if this pixel was already set
+                        if (pixel == 1) && self.Display.frame_buffer[fb] == 1 {
                             self.V[0xF] = 1;
                             self.Display.frame_buffer[fb] = 0;
-                        } else {
+                        } else if pixel == 1 {
                             self.Display.frame_buffer[fb] = 1;
                         }
                     }
@@ -247,7 +255,7 @@ impl Processor {
                     }
                     0xA1 => { // EXA1 - if(key()!=Vx)
                     }
-                    _ => println!("invalid opcode"),
+                    _ => println!("invalid opcode {}", op),
                 }
             }
             0xF => {
@@ -257,10 +265,10 @@ impl Processor {
                     0x0A => { // FX0A - Vx = get_key()
                     }
                     0x15 => { // FX15 - delay_timer(Vx)
-                        self.delay = self.V[ix];
+                        self.delay = self.V[x];
                     }
                     0x18 => { // FX18 - sound_timer(Vx)
-                        self.sound = self.V[ix];
+                        self.sound = self.V[x];
                     }
                     0x1E => { // FX1E - I +=Vx
                          // Most CHIP-8 interpreters' FX1E instructions do not affect VF, with one
@@ -278,10 +286,10 @@ impl Processor {
                     }
                     0x65 => { // FX65 - reg_load(Vx,&I)
                     }
-                    _ => println!("invalid opcode"),
+                    _ => println!("invalid opcode {}", op),
                 }
             }
-            _ => println!("invalid opcode"),
+            _ => println!("invalid opcode {} ", op),
         }
     }
 
